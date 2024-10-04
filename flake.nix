e659dd291bcf3adc7d981066b7fb09bf59c911f1
altogit <74936981+altogit@users.noreply.github.com>
2024-10-04 12:37:13 +1000

    Add files via upload
    Adding all the base files. To setup auto downloading and make further changes.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake for Master Ansible server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    oldpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs, oldpkgs}:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        MASTER-NIX = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            pkgs-18 = import oldpkgs {
+              system = system;
+              config.allowUnfree = true;
+            };
+          };
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
+    };
+}
