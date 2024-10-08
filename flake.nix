3517699dc838aa2250f0b83efe45010261c2c55b
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-08 12:13:18 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     oldpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    unstablepkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nixpkgs, oldpkgs}:
+  outputs = inputs@{ self, nixpkgs, oldpkgs, unstablepkgs}:
     let
       lib = nixpkgs.lib;
     in {
