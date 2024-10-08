c96398d1189cfd19158effbf50689684459e6431
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-08 17:24:14 +1100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
       root = {
         hashedPassword = "*";
       };
-      userSettings.username = {
+      "{$userSettings.username}" = {
         isNormalUser = true;
         description = userSettings.name;
         hashedPassword = "$6$gC/dArwhdt2So2tK$y.xbzqelEnKhR1xZbyZCjRd61R.c1lJrRxQRZPVB0dzEuAkOJ0v2ZtnTd1Fvsb0xi6KhdtSFIMuF86T4U.ohf1";
