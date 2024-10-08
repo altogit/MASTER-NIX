150680f19704a864af3fcb0fdcfc3da4c86ba7e1
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-08 13:50:55 +1100

    Update configuration.nix
    added firewall port for docker

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
   networking.networkmanager.enable = true;
 
   # Open ports in the firewall.
-   networking.firewall.allowedTCPPorts = [ 22 ];
+   networking.firewall.allowedTCPPorts = [ 22 3001 ];
    networking.firewall.allowedUDPPorts = [ ];
   # Or disable the firewall altogether.
    networking.firewall.enable = true;
