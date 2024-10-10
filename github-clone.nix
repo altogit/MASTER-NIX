bcaeb063622094cee0d48e5488fb2e83301dac7e
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-10 17:52:13 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -35,15 +35,15 @@ in
   config = mkIf cfg.enable {
 
     # Secrets handling: Write the token to a file in /etc with restricted permissions
-    environment.etc = listToAttrs (map (repo: {
-      name = repo.name;
-      value = {
-        source = repo.token;
-        mode = "0600";
-        user = repo.user;
-        group = repo.user;
-      };
-    }) cfg.repositories);
+    # environment.etc = listToAttrs (map (repo: {
+    #   name = repo.name;
+    #   value = {
+    #     source = repo.token;
+    #     mode = "0600";
+    #     user = repo.user;
+    #     group = repo.user;
+    #   };
+    # }) cfg.repositories);
 
     # Systemd services
     systemd.services = listToAttrs (map (repo: {
@@ -56,7 +56,8 @@ in
           Type = "oneshot";
           User = repo.user;
           Environment = [
-            "GITHUB_TOKEN_FILE=/etc/${repo.name}"
+            #"GITHUB_TOKEN_FILE=/etc/${repo.name}"
+            "GITHUB_TOKEN=${userSettings.gitHubPAT}"
             "REPO_URL=${repo.url}"
             "DESTINATION=${repo.destination}"
             "GIT=${pkgs.git}/bin/git"
@@ -65,7 +66,7 @@ in
             set -e
 
             # Read the GitHub token
-            GITHUB_TOKEN=$(cat "$GITHUB_TOKEN_FILE")
+            #GITHUB_TOKEN=$(cat "$GITHUB_TOKEN_FILE")
 
             # Prepare the URL with the token included
             AUTHENTICATED_URL="https://${repo.user}:${GITHUB_TOKEN}@${repo.url}"
