39a7fdab2834f49e532295e2aa9ec3bd6f1ffd2e
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-10 17:06:20 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         name = "Alto";
         hashedPassword = "$6$gC/dArwhdt2So2tK$y.xbzqelEnKhR1xZbyZCjRd61R.c1lJrRxQRZPVB0dzEuAkOJ0v2ZtnTd1Fvsb0xi6KhdtSFIMuF86T4U.ohf1";
         gitHubUser = "altogit";
-        gitHubPAT = "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"
+        gitHubPAT = "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";
       };
       systemSettings = {
         system = "x86_64-linux";
