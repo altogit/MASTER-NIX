8e487aafdfc3e51dac25305e5211c4761a133d27
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-10 19:08:49 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -76,21 +76,21 @@ in
           MASKED_URL="https://$repo.user:<token>@$repo.url"
 
           # Check if the repository exists
-          if [ -d "$repo.destination/.git" ]; then
+          if [ -d $repo.destination/.git ]; then
             echo "Updating repository at $repo.destination"
 
             # Update the remote URL to include the token
-            $GIT -C "$repo.destination" remote set-url origin $AUTHENTICATED_URL
+            $GIT -C $repo.destination remote set-url origin $AUTHENTICATED_URL
 
             # Pull with rebase
-            $GIT -C "$repo.destination" pull --rebase
+            $GIT -C $repo.destination pull --rebase
           else
-            echo "Cloning repository $MASKED_URL into $repo.destination"
-            $GIT clone $AUTHENTICATED_URL "$repo.destination"
+            echo Cloning repository $MASKED_URL into $repo.destination
+            $GIT clone $AUTHENTICATED_URL $repo.destination
           fi
 
           # Reset the remote URL to remove the token after pulling
-          $GIT -C "$repo.destination" remote set-url origin "https://$repo.url"
+          $GIT -C $repo.destination remote set-url origin https://$repo.url
           '
          '';
           # Ensure that the token is not exposed in the environment or logs
