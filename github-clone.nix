875093944168a481d82c5448c27a53b0cc048909
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-11 17:41:32 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -69,7 +69,9 @@ in
           ];
           ExecStart = ''
           ${pkgs.bash}/bin/sh -c "set -e; \
+          echo $GITHUB_TOKEN_FILE; \
           GITHUB_TOKEN=$(cat $GITHUB_TOKEN_FILE); \
+          echo the token: $GITHUB_TOKEN; \
           AUTHENTICATED_URL="https://$REPO_USER:$GITHUB_TOKEN@$REPO_URL"; \
           echo $AUTHENTICATED_URL; \
           MASKED_URL=https://$REPO_URL:<token>@$REPO_URL; \
