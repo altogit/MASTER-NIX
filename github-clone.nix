b2e473b17bd471274b77c94e74df48bad3b5a575
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-11 11:54:28 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -66,9 +66,21 @@ in
             "GIT=${pkgs.git}/bin/git"
             "REPO_USER=${repo.user}"
             "REPO_DESTINATION=${repo.destination}"
-            "SH=${pkgs.bash}/bin/sh"
           ];
-          ExecStart = ''${pkgs.bash}/bin/sh -c "echo the user $REPO_USER"'';
+          ExecStart = ''
+          ${pkgs.bash}/bin/sh -c "set -e; \
+          AUTHENTICATED_URL=https://$REPO_USER:$GITHUB_TOKEN@$REPO_URL; \
+          MASKED_URL=https://$REPO_URL:<token>@$REPO_URL; \
+          if [ -d $REPO_DESTINATION/.git ]; then \
+            echo Updating repository at $REPO_DESTINATION; \
+            $GIT -C $REPO_DESTINATION remote set-url origin $AUTHENTICATED_URL; \
+            $GIT -C $REPO_DESTINATION pull --rebase; \
+          else \
+            echo Cloning repository $MASKED_URL into $REPO_DESTINATION; \
+            $GIT clone $AUTHENTICATED_URL $REPO_DESTINATION; \
+          fi; \
+          $GIT -C $REPO_DESTINATION remote set-url origin https://$REPO_URL"
+          ''; 
           # Ensure that the token is not exposed in the environment or logs
           PassEnvironment = [];
         };
