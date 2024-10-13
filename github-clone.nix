520ecc694cca2cff2d2f213325616a3580f5a1f0
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-14 09:57:20 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -54,12 +54,12 @@ in
         after = [ "network-online.target" ];
         wants = [ "network-online.target" ];
         Environment = {
-          "GITHUB_TOKEN_file=${repo.token}";
-          "REPO_URL=${repo.url};"
-          DESTINATION=${repo.destination};
-          GIT=${pkgs.git}/bin/git;
-          REPO_USER=${repo.user};
-          REPO_DESTINATION=${repo.destination};
+          "GITHUB_TOKEN_file=${repo.token}"
+          "REPO_URL=${repo.url};
+          "DESTINATION=${repo.destination}"
+          "GIT=${pkgs.git}/bin/git"
+          "REPO_USER=${repo.user}"
+          "REPO_DESTINATION=${repo.destination}"
           };
         serviceConfig = {
           Type = "oneshot";
