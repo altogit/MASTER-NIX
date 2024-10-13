74c165027ebd4f7feb9101bdc656d0c88f732a86
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-14 09:56:23 +1100

    Update github-clone.nix

--- github-clone.nix
+++ github-clone.nix
@@ -54,9 +54,8 @@ in
         after = [ "network-online.target" ];
         wants = [ "network-online.target" ];
         Environment = {
-            #"GITHUB_TOKEN_FILE=/etc/${repo.name}"
-          GITHUB_TOKEN_file=${repo.token};
-          REPO_URL=${repo.url};
+          "GITHUB_TOKEN_file=${repo.token}";
+          "REPO_URL=${repo.url};"
           DESTINATION=${repo.destination};
           GIT=${pkgs.git}/bin/git;
           REPO_USER=${repo.user};
