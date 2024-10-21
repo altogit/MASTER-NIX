bc8459ef460a4753127bb6ca483c3747e33b5155
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-22 10:06:03 +1100

    system settings
    Added all the ansible schedules and assigned static IP address.

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,15 @@
   networking = {
     hostName = systemSettings.hostname;
     networkmanager.enable = true;
-
+    useDHCP = false;
+    interfaces.eth0.ipv4.addresses = [
+      {
+        address = "192.168.16.177";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = "192.168.16.254";
+    nameservers = [ "192.168.16.254" "1.1.1.1" ];
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 3001 ];
--- systemd-services.nix
+++ systemd-services.nix
@@ -47,4 +47,108 @@
       Persistent = true;
     };
   };
+    # Service that Updates servers.
+  systemd.services.ansibleUpdateMachines = {
+    description = "Update all the servers, weekly.";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ansible}/bin/ansible-playbook ./main.yml --inventory ../inventory --vault-password-file /home/${userSettings.username}/GH/vault.key";
+      User = userSettings.username;
+      WorkingDirectory = "/home/${userSettings.username}/Ansible/${userSettings.username}/updatesrv";
+      StandardOutput = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/updatesrv/patch.log";
+      StandardError = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/updatesrv/patch.log";
+      Environment = [
+        "HOME=/home/${userSettings.username}"
+        "PATH=${pkgs.openssh}/bin:${pkgs.ansible}/bin:${pkgs.coreutils}/bin:${pkgs.python3}/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
+  systemd.timers.ansibleUpdateMachines = {
+    description = "Timer for ansibleUpdateMachines.service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "Sun *-*-* 20:30";
+      Persistent = true;
+    };
+  };
+      # Service that Backup servers.
+  systemd.services.ansibleBackupMachines = {
+    description = "Backup all the servers, 1st of the month.";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ansible}/bin/ansible-playbook ./main.yml --inventory ../inventory --vault-password-file /home/${userSettings.username}/GH/vault.key";
+      User = userSettings.username;
+      WorkingDirectory = "/home/${userSettings.username}/Ansible/${userSettings.username}/backup1";
+      StandardOutput = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/backup1/patch.log";
+      StandardError = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/backup1/patch.log";
+      Environment = [
+        "HOME=/home/${userSettings.username}"
+        "PATH=${pkgs.openssh}/bin:${pkgs.ansible}/bin:${pkgs.coreutils}/bin:${pkgs.python3}/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
+  systemd.timers.ansibleBackupMachines = {
+    description = "Timer for ansibleBackupMachines.service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "1-*-* 00:30";
+      Persistent = true;
+    };
+  };
+        # Service that Backup servers.
+  systemd.services.ansibleBackup2Machines = {
+    description = "Backup all the servers, 15th of the month.";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ansible}/bin/ansible-playbook ./main.yml --inventory ../inventory --vault-password-file /home/${userSettings.username}/GH/vault.key";
+      User = userSettings.username;
+      WorkingDirectory = "/home/${userSettings.username}/Ansible/${userSettings.username}/backup2";
+      StandardOutput = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/backup2/patch.log";
+      StandardError = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/backup2/patch.log";
+      Environment = [
+        "HOME=/home/${userSettings.username}"
+        "PATH=${pkgs.openssh}/bin:${pkgs.ansible}/bin:${pkgs.coreutils}/bin:${pkgs.python3}/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
+  systemd.timers.ansibleBackup2Machines = {
+    description = "Timer for ansibleBackup2Machines.service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "15-*-* 00:30";
+      Persistent = true;
+    };
+  };
+          # Service gets the Tally app report.
+  systemd.services.ansibleTallyApp = {
+    description = "Generate the TallyApp report daily.";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ansible}/bin/ansible-playbook ./main.yml --inventory ../inventory --vault-password-file /home/${userSettings.username}/GH/vault.key";
+      User = userSettings.username;
+      WorkingDirectory = "/home/${userSettings.username}/Ansible/${userSettings.username}/tally-app-reports";
+      StandardOutput = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/tally-app-reports/patch.log";
+      StandardError = "append:/home/${userSettings.username}/Ansible/${userSettings.username}/tally-app-reports/patch.log";
+      Environment = [
+        "HOME=/home/${userSettings.username}"
+        "PATH=${pkgs.openssh}/bin:${pkgs.ansible}/bin:${pkgs.coreutils}/bin:${pkgs.python3}/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
+  systemd.timers.ansibleTallyApp = {
+    description = "Timer for ansibleTallyApp.service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 07:00";
+      Persistent = true;
+    };
+  };
 }
