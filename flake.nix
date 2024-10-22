db7638320cb6a901176a70f9ee438f9838c0f38c
FullMetalEd <42138052+FullMetalEd@users.noreply.github.com>
2024-10-22 11:37:03 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       userSettings = {
         username = "alto";
         name = "Alto";
-        hashedPassword = "$6$P4RDZVDICop4etRk$MTqE0nNFxCUnJFWUotnort0wEoGmIdmZF1tjMUCGe4J.4uYJ8E9ALUcri2kNCblvndMujf1Ubu/Jj1hHyfc.q";
+        hashedPassword = "$6$P4RDZVDICop4etRk$MTqE0nNFxCUnJFWUotnort0wEoGmIdmZF1tjMUCGe4J.4uYJ8E9ALUcri2kNCblvndMujf1Ubu/Jj1hHyfc.q/";
         gitHubUser = "altogit";
         gitHubPAT-File = "/home/alto/GH/ghapi";
       };
